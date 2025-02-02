016fa7251d4a5f0e6c1ef71795bc35eb9ec0afbc
[Juan <[juanenciso1007@gmail.com]>
2025-02-02 18:46:07 -0500

    [+] typeAnimal.controller.js: Maneja la excepcion que general Registrar Animal

--- src/controllers/typeAnimals.controllers.js
+++ src/controllers/typeAnimals.controllers.js
@@ -3,14 +3,24 @@ const TypeAnimalModel = require("../models/TypeAnimal.model");
 async function createAnimal(req, res){
     const inputData = req.body;
 
-    const data = await TypeAnimalModel.create(inputData);
+    try {
+        const data = await TypeAnimalModel.create(inputData);
 
-    res.json({
-        ok: true,
-        msg: 'Crea animal',
-        data: data
-    })
-};
+        res.json({
+            ok: true,
+            msg: 'Crea animal',
+            data: data
+        });
+
+    } catch (error) {
+        console.error(error);   //Error al desarrollador
+
+        res.json({
+            ok: false,
+            msg: 'Ha ocurrido una excepcion. El Animal no se ha registrado'
+        });
+    }
+}
 
 module.exports = {
     createAnimal
